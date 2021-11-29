7e2ba80e081ecce5a253c59de815b3c9e414e163
dmitry.doni <dmitry.doni@gmail.com>
2021-11-29 21:41:34 +0200

    Math functions, vectors, matrices

--- basic_functions.R
+++ basic_functions.R
@@ -72,4 +72,85 @@ LETTERS
 # Strings concatenation
 paste(letters, set="_", seq(1, 26))
 
-# ISOdatetime
+# Date functions
+ISOdate(2021, 11, 1:30)
+format(ISOdate(2021, 11, 1:30),"%d") # "%a", "%A", etc.
+
+# Tables
+d <- c(rep("a", 1), rep("b", 2), rep("c", 3))
+sample(d)
+table_d <- table(d)
+table_d
+
+# Math functions
+r <- seq(10, 1)
+r
+sum(r)
+mean(r)
+
+# Logical operators
+1==1
+6>7 | 9>8 # OR
+6>7 & 9>8 # AND
+
+# Public datasets
+data(package = .packages(all.available = TRUE))
+# install.packages("cluster")
+library(cluster)
+animals
+is.na(animals) # NA values will be marked TRUE
+head(animals, 5) # first 5 records
+animals$fly # select one column
+
+# Building datasets
+weight <- c(78, 66, 75, 87, 59)
+height <- c(178, 166, 175, 187, 159)
+sex <- c(rep("F", 3), rep("M", 2))
+
+df_1 <- data.frame(weight, height, sex, stringsAsFactors = F) # strings
+str(df_1) # structure
+df_1
+
+df_2 <- data.frame(weight, height, sex, stringsAsFactors = T) # sex is factor
+str(df_2) # structure
+df_2
+
+unique(sex)
+factor(sex)
+levels(factor(sex))
+
+# Building matrices
+
+# Sequence from 1 to 20
+m <- 1:20
+m
+class(m)
+dim(m) # NULL because it is a vector
+
+z <- c(10, 2)
+z
+
+dim(m) <- z  # m will be a matrix with dimensions specified in z
+m
+class(m) # m is now a matrix
+
+# Another way to create a matrix
+y <- 1:50
+y
+mtx <- matrix(y, 10, 5) # 10 rows, 5 columns
+mtx
+
+names_m = LETTERS[1:10]
+names_m
+
+# Set names for rows
+rownames(mtx, do.NULL = T, prefix = "row")
+rownames(mtx) <- names_m
+mtx
+
+# Set names for columns
+colnames(mtx, do.NULL = T, prefix = "col")
+colnames(mtx) <- paste0("day", set="_", 1:5)
+mtx
+mtx[, 2] # only the second column
+mtx[3, ] # only the third row
