cbbf5fd0819b9cfd27b3a86c742a057b1c21d860
dmitry.doni <dmitry.doni@gmail.com>
2021-12-10 19:02:48 +0200

    EDA draft

---
+++ exploratory_data_analysis.R
@@ -0,0 +1,6 @@
+# install.packages("cluster")
+library(cluster)  # load installed package
+data()  # view data sets in package ‘cluster’
+?votes.repub  # view the description of the data set
+
+# work in progress
