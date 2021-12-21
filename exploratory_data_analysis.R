622d4c905345fc422a8a6dd8e207a8668f7d47b9
dmitry.doni <dmitry.doni@gmail.com>
2021-12-21 19:44:36 +0200

    Visualize as boxplots, calculate percentiles

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -37,5 +37,55 @@ head(cardio, 3)
 mypar(1, 2)  # plot properties: 2 charts on 1 row
 hist(cardio$ap_hi)  # not symmetrical shape (left skew)
 hist(cardio$ap_lo)  # not symmetrical shape (left skew)
+
+# Descriptive statistics
+mean(cardio$ap_lo)  # 96.63
+sd(cardio$ap_lo)  # 188.47 - it means that variance is high
 mean(cardio$ap_hi)  # 128.82
 sd(cardio$ap_hi)  # 154.01 - it means that variance is high
+
+# Visualize as boxplots
+box_lo <- boxplot(cardio$ap_lo)  # there are outliers
+box_hi <- boxplot(cardio$ap_hi)  # there are outliers
+
+# Visualize as boxplots (filter out the outliers)
+box_lo <- boxplot(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])
+title("Low Pressure")
+box_hi <- boxplot(cardio$ap_hi[cardio$ap_hi<300 & cardio$ap_hi>40])
+title("High Pressure")
+
+# Find other outliers
+min(cardio$ap_hi)  # -150
+cardio$ap_hi[cardio$ap_hi<0]  # -100 -115 -100 -140 -120 -150 -120
+
+# Percentiles: median (50%), 1st quartile (25%), 3rd quartile (75%)
+median(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])  # 80
+quantile(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20], 0.25)  # 25%, 80
+quantile(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20], 0.75)  # 75%, 90
+
+# Calculate the 1st quartile (25%) manually
+# Sort all values
+sort(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])
+# Count the values
+cnt <- length(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])
+cnt  # 68994
+# 25 percent of values
+cnt_p25 <- cnt * 0.25
+cnt_p25  # 17248.5
+# Index of 25th percentile
+idx_p25 <- trunc(cnt_p25) + 1  # truncate the fractional part and add 1
+idx_p25  # 17249
+# Sort all values and get the value of the element with the index
+p25 <- sort(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])[idx_p25]
+p25  # 80
+
+# Calculate the median (50%) manually
+idx_p50_n1 <- cnt/2
+idx_p50_n1  # 34497
+idx_p50_n2 <- cnt/2 + 1
+idx_p50_n2  # 34498
+sort(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])[c(idx_p50_n1, idx_p50_n2)]  # 80 80
+# First 10
+head(sort(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])[idx_p25:idx_p50_n2], 10)  # 80 80 80 ...
+# Last 10
+tail(sort(cardio$ap_lo[cardio$ap_lo<200 & cardio$ap_lo>20])[idx_p25:idx_p50_n2], 10)  # 80 80 80 ...
